fd9c0af5a9d146ad4b07e631c012b41d6d05f15e
Rodolfo P A <6721075+rodoufu@users.noreply.github.com>
2025-01-08 18:41:48 -0300

    Fix issue with colors

--- contrib/bluetooth_battery.py
+++ contrib/bluetooth_battery.py
@@ -2,23 +2,7 @@ import argparse
 import json
 import subprocess
 import sys
-from enum import Enum
-from typing import List
-
-
-class bcolors(Enum):
-    HEADER = "\033[95m"
-    OKBLUE = "\033[94m"
-    OKCYAN = "\033[96m"
-    OKGREEN = "\033[92m"
-    WARNING = "\033[93m"
-    FAIL = "\033[91m"
-    ENDC = "\033[0m"
-    BOLD = "\033[1m"
-    UNDERLINE = "\033[4m"
-
-    def colored(self, text: str) -> str:
-        return f"{self.value}{text}{self.ENDC.value}"
+from typing import List, Optional
 
 
 def call_process_get_output(cli: List[str]) -> str:
@@ -28,12 +12,27 @@ def call_process_get_output(cli: List[str]) -> str:
     return output.decode()
 
 
+class I3Module:
+    def __init__(
+        self,
+        full_text: str,
+        name: str,
+        color: Optional[str] = None,
+        markup: Optional[str] = None,
+    ):
+        self.full_text = full_text
+        self.name = name
+        if color:
+            self.color = color
+        if markup:
+            self.markup = markup
+
+
 def get_bluetooth_battery(
     is_debug: bool,
     battery_red: int,
     battery_yellow: int,
-    ignore_colors: bool,
-) -> List[str]:
+) -> List[I3Module]:
     devices_output = call_process_get_output(["bluetoothctl", "devices"]).splitlines()
     device_name_id_map = {
         x[1].strip(): x[0].strip()
@@ -43,7 +42,7 @@ def get_bluetooth_battery(
         )
     }
 
-    line = []
+    resp = []
     for device_name, device_id in device_name_id_map.items():
         device_info = call_process_get_output(
             ["bluetoothctl", "info", device_id]
@@ -81,6 +80,7 @@ def get_bluetooth_battery(
         if icon:
             icon = icon_name_symbol.get(icon)
 
+        color = None
         if is_connected and battery is not None:
             battery_text = f"{battery:.0f}%"
             if icon:
@@ -89,19 +89,21 @@ def get_bluetooth_battery(
                 battery_text = f"{device_name} {battery_text}"
 
             if battery_red <= battery <= battery_yellow:
-                if ignore_colors:
-                    battery_text = f"{battery_text} 󰥄"
-                else:
-                    battery_text = bcolors.WARNING.colored(battery_text)
+                color = "#FFFF00"
+                battery_text = f"{battery_text} 󰥄"
             elif battery < battery_red:
-                if ignore_colors:
-                    battery_text = f"{battery_text} 󰤾"
-                else:
-                    battery_text = bcolors.FAIL.colored(battery_text)
+                color = "#FF0000"
+                battery_text = f"{battery_text} 󰤾"
 
-            line.append(battery_text)
+            resp.append(
+                I3Module(
+                    full_text=battery_text,
+                    name=f"bluetooth_battery_{device_name}",
+                    color=color,
+                )
+            )
 
-    return line
+    return resp
 
 
 def print_line(message):
@@ -128,29 +130,21 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--debug", "-d", help="Run in debug mode", action="store_true")
     parser.add_argument(
-        "--ignore-colors", help="Ignore printing using colors", action="store_true"
+        "--battery-red", help="Battery level to print red", type=int, default=20
     )
     parser.add_argument(
-        "--battery_red", help="Battery level to print red", type=int, default=20
-    )
-    parser.add_argument(
-        "--battery_yellow", help="Battery level to print red", type=int, default=50
+        "--battery-yellow", help="Battery level to print red", type=int, default=50
     )
     args = parser.parse_args()
 
-    # FIXME: the colors are not working anymore in the status bar
-
     if args.debug:
-        content = " ".join(
-            get_bluetooth_battery(
-                is_debug=args.debug,
-                battery_yellow=args.battery_yellow,
-                battery_red=args.battery_red,
-                ignore_colors=args.ignore_colors,
-            )
+        i3_modules = get_bluetooth_battery(
+            is_debug=args.debug,
+            battery_yellow=args.battery_yellow,
+            battery_red=args.battery_red,
         )
         print(f"args: {args}")
-        print(content)
+        print(i3_modules)
     else:
         # Skip the first line which contains the version header.
         print_line(read_line())
@@ -164,24 +158,17 @@ if __name__ == "__main__":
             if line.startswith(","):
                 line, prefix = line[1:], ","
 
-            content = " ".join(
-                get_bluetooth_battery(
-                    is_debug=args.debug,
-                    battery_yellow=args.battery_yellow,
-                    battery_red=args.battery_red,
-                    ignore_colors=args.ignore_colors,
-                )
+            i3_modules = get_bluetooth_battery(
+                is_debug=args.debug,
+                battery_yellow=args.battery_yellow,
+                battery_red=args.battery_red,
             )
 
             j = json.loads(line)
             # insert information into the start of the json, but could be anywhere
             # CHANGE THIS LINE TO INSERT SOMETHING ELSE
-            j.insert(
-                0,
-                {
-                    "full_text": "%s" % content,
-                    "name": "bluetooth_battery",
-                },
-            )
+
+            for i3_module in i3_modules:
+                j.insert(0, i3_module.__dict__)
             # and echo back new encoded json
             print_line(prefix + json.dumps(j))
